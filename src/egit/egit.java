37a3e963d531553d83652da264b62099caa79685
Vespertino <Vespertino@C8PC22>
2018-05-18 18:45:48 +0200

    Prueba tercera version

--- src/egit/egit.java
+++ src/egit/egit.java
@@ -4,7 +4,8 @@ public class egit {
 
 	public static void main(String[] args) {
 		System.out.println("Esta clase es para la prueba de EGit");
-
+		System.out.println("Esta clase es para la segunda prueba de Egit");
+		System.out.println("Esta clase es la tercera prueba de EGIT");
 	}
 
 }
