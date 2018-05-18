ba35d333f09f90e91dc0d6e3d80c06f81e9cceb0
Vespertino <Vespertino@C8PC23>
2018-05-18 18:16:22 +0200

    modificado por ruben

--- src/egit/egit.java
+++ src/egit/egit.java
@@ -3,7 +3,7 @@ package egit;
 public class egit {
 
 	public static void main(String[] args) {
-		System.out.println("Esta clase es para la prueba de EGit");
+		System.out.println("Esta clase es para la prueba de EGit modificada por ruben");
 
 	}
 
